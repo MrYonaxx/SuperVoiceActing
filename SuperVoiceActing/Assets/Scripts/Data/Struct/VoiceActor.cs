28e65bbd05341321a34fbe66102ea3adc5989f53
Nam Mirbeau-baudin <nam.mirbeau@gmail.com>
2019-05-02 15:50:59 +0200

    opti + reglage

--- SuperVoiceActing/Assets/Scripts/Data/Struct/VoiceActor.cs
+++ SuperVoiceActing/Assets/Scripts/Data/Struct/VoiceActor.cs
@@ -304,7 +304,6 @@ namespace VoiceActing
             List<int> possibilities = new List<int>(8) { 0, 1, 2, 3, 4, 5, 6, 7 };
             int choiceEmotion = 0;
             choiceEmotion = Random.Range(0, possibilities.Count);
-            Debug.Log(choiceEmotion);
 
             while (pointCumul != 0)
             {
