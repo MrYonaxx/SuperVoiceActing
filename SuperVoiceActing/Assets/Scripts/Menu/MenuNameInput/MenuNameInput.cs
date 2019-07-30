63456c950123e1107a90c65b3cff21af849f7d45
Nam Mirbeau-baudin <nam.mirbeau@gmail.com>
2019-07-31 00:31:21 +0200

    name studio

---
+++ SuperVoiceActing/Assets/Scripts/Menu/MenuNameInput/MenuNameInput.cs
@@ -0,0 +1,69 @@
+﻿/*****************************************************************
+ * Product:    #PROJECTNAME#
+ * Developer:  #DEVELOPERNAME#
+ * Company:    #COMPANY#
+ * Date:       #CREATIONDATE#
+******************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace VoiceActing
+{
+	public class MenuNameInput : MonoBehaviour
+	{
+        #region Attributes 
+
+        /* ======================================== *\
+         *               ATTRIBUTES                 *
+        \* ======================================== */
+
+
+        [SerializeField]
+        PlayerData playerData;
+        [SerializeField]
+        TMPro.TMP_InputField inputField;
+        [SerializeField]
+        StoryEventData storyEventData;
+        [SerializeField]
+        string nextScene;
+
+        #endregion
+
+        #region GettersSetters 
+
+        /* ======================================== *\
+         *           GETTERS AND SETTERS            *
+        \* ======================================== */
+
+
+        #endregion
+
+        #region Functions 
+
+        /* ======================================== *\
+         *                FUNCTIONS                 *
+        \* ======================================== */
+
+        public void RegisterPlayerName()
+        {
+            playerData.PlayerName = inputField.text;
+            playerData.NextStoryEvents.Add(storyEventData);
+            SceneManager.LoadScene(nextScene, LoadSceneMode.Single);
+        }
+
+        public void RegisterStudioName()
+        {
+            playerData.StudioName = inputField.text;
+            playerData.NextStoryEvents.Add(storyEventData);
+            SceneManager.LoadScene(nextScene, LoadSceneMode.Single);
+        }
+
+        #endregion
+
+    } // MenuNameInput class
+	
+}// #PROJECTNAME# namespace
